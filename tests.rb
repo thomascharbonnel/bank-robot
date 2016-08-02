e8867ea8c4fa3a52d4fda9c17a32a3c47e36798b
Asche42 <thomas@charbonnel.eu>
2016-08-02 12:08:23 +0200

    Tests went from unit to spec

--- tests.rb
+++ tests.rb
@@ -5,14 +5,17 @@ require 'minitest/autorun'
 require_relative 'exceptions'
 require_relative 'bank_interface'
 
-class TestBankInterface < Minitest::Test
-  def setup
+describe StockExchangeRobot::BankInterface do
+  before do
     @bi = StockExchangeRobot::BankInterface.new
   end
 
-  def test_abstract_bank_interface_should_throw_exception
-    assert_raises(StockExchangeRobot::BankInterface::UnimplementedBankInterfaceException) { @bi.connect }
-    assert_raises(StockExchangeRobot::BankInterface::UnimplementedBankInterfaceException) { @bi.index_quotes }
-    assert_raises(StockExchangeRobot::BankInterface::UnimplementedBankInterfaceException) { @bi.show_quote(nil) }
+  describe "when calling abstract methods from BankInterface" do
+    it "should raise an UnimplementedBankInterfaceException exception" do
+      # All three methods must raise the exception.
+      assert_raises(StockExchangeRobot::BankInterface::UnimplementedBankInterfaceException) { @bi.connect }
+      assert_raises(StockExchangeRobot::BankInterface::UnimplementedBankInterfaceException) { @bi.index_quotes }
+      assert_raises(StockExchangeRobot::BankInterface::UnimplementedBankInterfaceException) { @bi.show_quote(nil) }
+    end
   end
 end
