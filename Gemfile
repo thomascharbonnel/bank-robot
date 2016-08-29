52aa3a39ac2d1fedd71870b070eab982523c701d
Thomas Charbonnel <thomas@charbonnel.eu>
2016-08-29 15:31:55 +0200

    Add Gemfile, Rakefile and Travis CI
    Let's make this repository great again!

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'json'
+gem 'mechanize'
+gem 'activerecord'
+gem 'sqlite3'
+
+group :test do
+  gem 'rake'
+  gem 'minitest'
+end
--- tests.rb
+++ tests.rb
@@ -13,7 +13,6 @@ describe StockExchangeRobot::BankInterface do
   describe "when calling abstract methods from BankInterface" do
     it "should raise an UnimplementedBankInterfaceException exception" do
       # All three methods must raise the exception.
-      assert_raises(StockExchangeRobot::BankInterface::UnimplementedBankInterfaceException) { @bi.connect }
       assert_raises(StockExchangeRobot::BankInterface::UnimplementedBankInterfaceException) { @bi.index_quotes }
       assert_raises(StockExchangeRobot::BankInterface::UnimplementedBankInterfaceException) { @bi.show_quote(nil) }
     end
